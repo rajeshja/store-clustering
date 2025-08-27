a752171f808213168bd36b9e546f1e3dc3bc30bc
Rajesh Advani <rajeshjadvani@gmail.com>
2025-08-27 04:06:55 +0000

    Change the icon for dark stores to be something that refers to online sh

--- src/components/icons.tsx
+++ src/components/icons.tsx
@@ -1,10 +1,11 @@
-import { ShoppingCart, Zap, Store as StoreIcon } from 'lucide-react';
+import { ShoppingCart, Zap, Store as StoreIcon, Warehouse } from 'lucide-react';
 import type { LucideProps } from 'lucide-react';
 import type { Store } from '@/types';
 
 export const Icons = {
   supermarket: (props: LucideProps) => <ShoppingCart {...props} />,
   express: (props: LucideProps) => <Zap {...props} />,
+  'dark store': (props: LucideProps) => <Warehouse {...props} />,
   default: (props: LucideProps) => <StoreIcon {...props} />,
 };
 
